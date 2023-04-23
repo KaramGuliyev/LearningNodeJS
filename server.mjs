3e89d08b7a21d969717c6457f61fe3713d426243
Karam Guliyev <karamguliyev10@gmail.com>
2023-04-24 00:54:19 +0300

    7 - Use a Method with a Callback

--- server.mjs
+++ server.mjs
@@ -1,10 +1,16 @@
-import figlet from "figlet";
+// Create a script that uses the Node.js core fs.writeFile() (callback API) method to write a text file.
+// The documentation for this method is on the Node.js File system page.
 
-figlet("HABİBİ ALERT", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
+import { readFile } from "node:fs";
+
+readFile(
+  "message.txt",
+  "utf8",
+  (err, data) => {
+    callback(err, data);
   }
+);
+
+function callback(err, data) {
   console.log(data);
-});
+}
