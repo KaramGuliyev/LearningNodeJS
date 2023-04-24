a6d0fd9159aee1b3e746a919ad190ad72f151a62
Karam Guliyev <karamguliyev10@gmail.com>
2023-04-24 15:18:49 +0300

    Set up a simple Express App

---
+++ server.ts
@@ -0,0 +1,41 @@
+// Configure your app (app.use()) to:
+// accept JSON from the Client
+// log the Client's requests
+
+import express from "express";
+import morgan from "morgan";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.listen(port, () => {
+  console.log(
+    `Server is running on port http://localhost:${port}`
+  );
+});
+
+app.get("/planets", (req, res) => {
+  res.status(200).json(planets)
+})
+
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
