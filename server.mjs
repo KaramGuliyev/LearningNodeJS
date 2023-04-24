b3eccb8ea24531682c58894f0313aa797c2c0679
Karam Guliyev <karamguliyev10@gmail.com>
2023-04-24 12:56:57 +0300

    Fix

--- server.mjs
+++ server.mjs
@@ -1,16 +1,11 @@
-// Create a script that uses the Node.js core fs.writeFile() (callback API) method to write a text file.
-// The documentation for this method is on the Node.js File system page.
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
 
-import { readFile } from "node:fs";
+const data = new Uint8Array(Buffer.from('Habibi text file'));
 
-readFile(
-  "message.txt",
-  "utf8",
-  (err, data) => {
-    callback(err, data);
-  }
-);
-
-function callback(err, data) {
-  console.log(data);
+function callback(err) {
+  if (err) throw err;
+  console.log('The file has been saved!');
 }
+
+writeFile('habibi.txt', data, callback);
